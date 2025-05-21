c1a208d4185e93347e42ad725b3a274866610328
W4vyCreative <lucaschecchinato@gmail.com>
2025-05-21 18:52:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 hf_token = st.secrets["HF_TOKEN"]
 
-st.title("AI Agents Quest")
+st.set_page_config(page_title="Gamified AI Agent", page_icon="🤖", layout="wide")
+st.title("AI Agents Quest: Learning how to create your own AI agents in a gamified way!")
+st.write("Welcome! This APP will guide you on building your own **AI** agents " "utilizing interactive challenges. Let's go!")
 st.button("Press here")
-st.sidebar.info("This is a sidebar")
+
+st.sidebar.info("This is a sidebar
