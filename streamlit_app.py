a6a56e4ea9e06587b534d8f3ba1e78b7244dc61d
W4vyCreative <lucaschecchinato@gmail.com>
2025-05-21 18:58:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,11 @@ hf_token = st.secrets["HF_TOKEN"]
 st.set_page_config(page_title="Gamified AI Agent", page_icon="🤖", layout="wide")
 st.title("AI Agents Quest: Learning how to create your own AI agents in a gamified way!")
 st.write("Welcome! This APP will guide you on building your own **AI agents** " "utilizing interactive challenges. Let's go!")
+
+st.text_input("Insert your name here: ")
+name = st.text_input
+
 st.button("Press here")
+return ("Welcome, " + name + "!")
 
 st.sidebar.info("This is a sidebar")
