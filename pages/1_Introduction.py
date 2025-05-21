0565230d34b4a16864413667e4ae79e261dc552d
W4vyCreative <lucaschecchinato@gmail.com>
2025-05-21 19:47:28 -0300

    Update 1_Introduction.py

--- pages/1_Introduction.py
+++ pages/1_Introduction.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title("📚 Introduction")
-st.write("This is the first phase of your AI agent training")
+st.title("📚 Introduction Module")
+st.write("This is the first phase of your AI agent training. Here, you will learn the basic concepts about AI agents, course content etc.")
+st.write("Use the side menu to navigate between the app pages")
