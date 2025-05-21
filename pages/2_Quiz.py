4ee2f71a96e5bb67062497af9d0215e7cb58b980
W4vyCreative <lucaschecchinato@gmail.com>
2025-05-21 19:49:23 -0300

    Update 2_Quiz.py

--- pages/2_Quiz.py
+++ pages/2_Quiz.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.title("🤔 Quiz")
-st.write("Let's test your knowledge!")
+st.write("Let's test your knowledge!: ")
+
+st.markdown("**QUESTION:** Which programming language are we using to build this app?")
+st.markdown("Alternative: A) Java, B) Python, C) CU")
