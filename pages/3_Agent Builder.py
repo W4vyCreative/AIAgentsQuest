8cb17c70a81be62ada31ff5412236b41d743a35a
W4vyCreative <lucaschecchinato@gmail.com>
2025-05-21 19:53:31 -0300

    Update 3_Agent Builder.py

--- pages/3_Agent Builder.py
+++ pages/3_Agent Builder.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("🤖 Agent Builder")
 st.write("Let's start building your own agent!")
+
+st.image("https://giphy.com/gifs/kencarson-agc-ken-carson-fighting-my-demons-7HJANHFYARttjaugTu")
