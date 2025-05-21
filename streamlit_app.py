a2a11a5a39b5e78cdc8c57f2b260565a9ec7f715
W4vyCreative <lucaschecchinato@gmail.com>
2025-05-21 19:09:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,12 @@ st.set_page_config(page_title="Gamified AI Agent", page_icon="🤖", layout="wid
 st.title("AI Agents Quest: Learning how to create your own AI agents in a gamified way!")
 st.write("Welcome! This APP will guide you on building your own **AI agents** " "utilizing interactive challenges. Let's go!")
 
-st.text_input("Insert your name here: ")
-name = st.text_input
+st.text_input("Insert your name here:", key="username")
+name = 
 
 st.button("Press here")
-def button(name):
-    if st.button(pressed): 
-        st.write("Welcome, " + name + "!")
+    if st.button("Press here") = True: 
+        st.write("Welcome,", st.session_state.username + "!")
     else:
         st.button("Press here")
 st.sidebar.info("This is a sidebar")
