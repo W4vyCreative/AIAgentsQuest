5d0cc48e024bb6c27c2541cbcb1e781fe8a1bd2c
W4vyCreative <lucaschecchinato@gmail.com>
2025-05-21 19:54:50 -0300

    Update 3_Agent Builder.py

--- pages/3_Agent Builder.py
+++ pages/3_Agent Builder.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("🤖 Agent Builder")
 st.image("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNnBzcmF5ZjJuM3JpNDRsNjZrcXN3cHMxbWJqYjZoMWhkN21mdjVzYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7HJANHFYARttjaugTu/giphy.gif")
 
-st.write("Let's start building your own agent!")
+st.write("Let's start building your own AI agent!")
