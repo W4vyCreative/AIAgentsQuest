19fb31169fd2abf36a7caccc6496e8263bf52c3c
W4vyCreative <lucaschecchinato@gmail.com>
2025-05-22 19:19:03 -0300

    Update 2_Quiz.py

--- pages/2_Quiz.py
+++ pages/2_Quiz.py
@@ -1,7 +1,21 @@
 import streamlit as st
 
+if "xp" not in st.session_state:
+  session_state["xp"] = 0
+
 st.title("🤔 Quiz")
 st.write("Let's test your knowledge!: ")
 
-st.markdown("**QUESTION:** Which programming language are we using to build this app?")
-st.markdown("Alternatives: A) Java, B) Python, C) CU")
+st.write("Practice your knowldege answering the question: ")
+question = "**QUESTION:** Which programming language are we using to build this app?"
+choices = ["A) Java", "B) Python", "C) CU"]
+right_choice = ["B) Python"]
+
+choice = st.radio(question, choices)
+
+if st.button("confirm answer"):
+   if choice == right_choice:
+      st.session_state["xp"] += 10
+      st.success(f"✅Congratulations, you are right! +10xp.(total XP = {st.session_state ["xp"]}")
+else:
+   st.error("❌Oh, you are wrong 😔... Try again!")
