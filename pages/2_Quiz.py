af1bbc76d8c1bf3a742be800da950104519cb555
W4vyCreative <lucaschecchinato@gmail.com>
2025-05-22 19:05:35 -0300

    Update 2_Quiz.py

--- pages/2_Quiz.py
+++ pages/2_Quiz.py
@@ -4,4 +4,4 @@ st.title("🤔 Quiz")
 st.write("Let's test your knowledge!: ")
 
 st.markdown("**QUESTION:** Which programming language are we using to build this app?")
-st.markdown("Alternative: A) Java, B) Python, C) CU")
+st.markdown("Alternatives: A) Java, B) Python, C) CU")
