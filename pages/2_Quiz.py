efe2a9e0e33779e4565596ac622d263632604311
W4vyCreative <lucaschecchinato@gmail.com>
2025-05-22 19:21:18 -0300

    Update 2_Quiz.py

--- pages/2_Quiz.py
+++ pages/2_Quiz.py
@@ -9,7 +9,7 @@ st.write("Let's test your knowledge!: ")
 st.write("Practice your knowldege answering the question: ")
 question = "**QUESTION:** Which programming language are we using to build this app?"
 choices = ["A) Java", "B) Python", "C) CU"]
-right_choice = ["B) Python"]
+right_choice = "B) Python"
 
 choice = st.radio(question, choices)
 
